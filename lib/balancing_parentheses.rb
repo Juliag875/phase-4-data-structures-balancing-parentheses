b4a2155e792585b456ffa9d9273d464eb81ffff9
Iuliia El Sammak <juliag875@gmail.com>
2022-04-24 21:05:17 -0400

    commit tree 9506f39075999fb10e12d739ae04b3a0f573bd39

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,19 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(str)
+  # create instance of Stack class
+  stack = Stack.new 
+  # checks the string to determine whether the parentheses inside it are balanced
+  str.chars.each do |c|
+    if stack.peek == '(' && c == ')'
+      stack.pop
+    else
+      stack.push(c)
+    end
+  end
+  stack.size
+end
+
+
+# Balanced: (), (()())
+# Not balanced: ())(), )
